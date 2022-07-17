ac00f4ced99f516b765df77637506df3fbd4d6c3
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-07-17 22:54:54 +0530

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-   // for (int i = 0 ; i <= n ; i++){
-    //     for (int j = 0 ; j <= n-i-1 ; j++ ){
-    //         cout << " ";
-    //     }cout << endl; 
-    //     for (int j = 0 ; j <= i ; j++ ){
-    //         cout << "*";
-    //     }cout << endl; 
-    // }
